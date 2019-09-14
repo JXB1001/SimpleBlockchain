7da3367ce9982ca76a196ee3bdc9f78f964fe8f2
Jacob Bentley <bentley.silas@gmail.com>
2019-09-14 13:16:36 +0100

    Creating the outlines for the Block and BlockChain classes

---
+++ Block.py
@@ -0,0 +1,9 @@
+class Block(object):
+    def __init__():
+        pass
+    #initial structure of the block class 
+    def compute_hash():
+        pass
+    #producing the cryptographic hash of each block 
+  
+

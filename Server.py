623df0d7f48d1d09a1d03ade35dc8d368aa5d38f
Jacob Bentley <bentley.silas@gmail.com>
2019-09-15 12:42:53 +0100

    Structure Updates

--- Server.py
+++ Server.py
@@ -3,12 +3,17 @@
 # - mine a new block
 # - return the entire chain
 
+import Chain
 from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return "Block Chain Fun!"
+
+@app.route("/chain")
+def getChain():
+    return "myChain"
 
 if __name__ == "__main__":
     app.run()
